63a57d8eba000e1a6a1a9a7e0b65e4093ecbe083
Alex Chistyakov <alexclear@gmail.com>
2017-04-28 00:10:04 +0300

    Deploying basically works

--- lib/Deployka.pm
+++ lib/Deployka.pm
@@ -21,24 +21,7 @@ const our $option_password => 'password';
 const our $option_application => 'application';
 const our $option_timeout => 'timeout';
 
-sub deploy {
-    my %options = @_;
-    my %config;
-    if((exists $options{$option_config}) && (-f ${$options{$option_config}})) {
-        %config = parse_config(${$options{$option_config}});
-    }
-    if(exists $options{$option_hostname}) {
-        $config{$option_hostname} = ${$options{$option_hostname}};
-    }
-    if(exists $options{$option_application}) {
-        $config{$option_application} = ${$options{$option_application}};
-    }
-    if($config{$option_hostname} eq 'localhost') {
-        copy($config{$option_application}, "/opt/tomcat8/webapps/") or die "Copy failed: $!";
-    }
-}
-
-sub undeploy {
+sub parse_config {
     my %options = @_;
     my %config;
     if((exists $options{$option_config}) && (-f ${$options{$option_config}})) {
@@ -62,6 +45,28 @@ sub undeploy {
     if(exists $options{$option_password}) {
         $config{$option_password} = ${$options{$option_password}};
     }
+    return %config;
+}
+
+sub deploy {
+    my %config = parse_config( @_ );
+    my $ua = LWP::UserAgent->new;
+    $ua->timeout($config{$option_timeout});
+    $ua->credentials($config{$option_hostname} . ":" . $config{$option_port}, "Tomcat Manager Application", $config{$option_user}, $config{$option_password});
+    open FILE, $config{$option_application} || die $!;
+    binmode FILE;
+    my $content = do { local $/; <FILE> };
+    close FILE;
+    my $response = $ua->put("http://" . $config{$option_hostname} . ":" . $config{$option_port} . "/manager/text/deploy?update=true&path=/testwebapp-1", Content => $content);
+    if ($response->is_success) {
+        print("Deployed!\n");
+    } else {
+        die "Deploying failed: " . $response->status_line . "\n";
+    }
+}
+
+sub undeploy {
+    my %config = parse_config( @_ );
     my $ua = LWP::UserAgent->new;
     $ua->timeout($config{$option_timeout});
     $ua->credentials($config{$option_hostname} . ":" . $config{$option_port}, "Tomcat Manager Application", $config{$option_user}, $config{$option_password});
